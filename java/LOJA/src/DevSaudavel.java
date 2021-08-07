27dfe0fcd5b574ca869cc26f86e389ce552c6b46
unknown <giovanna.penedos@outlook.com>
2021-08-07 14:19:47 -0300

    Atualização

--- java/LOJA/src/DevSaudavel.java
+++ java/LOJA/src/DevSaudavel.java
@@ -1,26 +1,42 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class DevSaudavel {
 
 	public static void main(String[] args) {
 		
 		Scanner ler = new Scanner(System.in);
+		
+		//arredondamento do valor
+		DecimalFormat df = new DecimalFormat("#.00");
+		
+		data/hora atual
+		LocalDateTime agora = LocalDateTime.now();
+		// formatar a data
+		DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+		String dataFormatada = formatterData.format(agora);
+		// formatar a hora
+		DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+		String horaFormatada = formatterHora.format(agora);
+		
+
+		
 
 		//Compra finalizada, mostrando o carrinho final 
 		tituloCarrinho();
 		for (contador = 0; contador < 10; contador++) {
 			if (carrinhoCompras[contador] != 0) {
 				System.out.printf("♥     "+codProduto[contador]+" \t♥\t"+produto[contador]+"\t♥\t"+preco[contador]+"\t   ♥\t  "+carrinhoCompras[contador]+"\t♥\t"+(carrinhoCompras[contador] * preco[contador])+"\t    ♥\n");
-				total += carrinhoCompras[contador] * preco[contador]
+				total += carrinhoCompras[contador] * preco[contador];
 			}
 		}
 		linha(falso)
 		System.out.print("\nVALOR DA COMPRA : %f",total);
 		System.out.println("\n\nFormas de Pagamento : \nOpção [1] - Dinheiro á vista c/ 10% de desconto\nOpção [2] - Cartão á vista c/ 10% de acréscimo\nOpção [3] - Cartão em 2x c/ 15% de acréscimo\n\nDigite a opção de pagamento : ");
 		opcao = ler.nextInt();
-		linhaNota(verdadeiro)
+		linhaNota(verdadeiro);
 		
 		//opções de pagamento e emissão das nostas fiscais
 		while (opcao >3 ou opcao <=0) {
@@ -37,17 +53,9 @@ public class DevSaudavel {
 				}
 			}
 			System.out.print("\n\nDinheiro à vista");
-			System.out.println("9% de ICMS : R$ "+((total * 0.09),2));
-			System.out.println("10% de desconto : R$ "+((total * 0.1),2));
-			System.out.println("\nTOTAL DA COMPRA R$ "+((total * 0.9),2),"\n");
-			// data/hora atual
-			LocalDateTime agora = LocalDateTime.now();
-			// formatar a data
-			DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-			String dataFormatada = formatterData.format(agora);
-			// formatar a hora
-			DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
-			String horaFormatada = formatterHora.format(agora);
+			System.out.println("9% de ICMS : R$ "+df.format(total * 0.09));
+			System.out.println("10% de desconto : R$ "+df.format(total * 0.1));
+			System.out.println("\nTOTAL DA COMPRA R$ "+df.format(total * 0.9),"\n");
 			System.out.print("Data: "+formatterData.format(agora)+"\t");
 			System.out.print("Hora: "+formatterHora.format(agora));
 			linhaNota(falso);
@@ -62,17 +70,9 @@ public class DevSaudavel {
 				}
 			}
 			System.out.print("\n\nCartão à vista");
-			System.out.println("9% de ICMS : R$ "+((total * 0.09),2));
-			System.out.println("10% de acréscimo : R$ "+((total * 0.1),2));
-			System.out.println("\nTOTAL DA COMPRA R$ "+((total * 1.1),2),"\n");
-			// data/hora atual
-			LocalDateTime agora = LocalDateTime.now();
-			// formatar a data
-			DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-			String dataFormatada = formatterData.format(agora);
-			// formatar a hora
-			DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
-			String horaFormatada = formatterHora.format(agora);
+			System.out.println("9% de ICMS : R$ "+df.format(total * 0.09));
+			System.out.println("10% de acréscimo : R$ "+df.format(total * 0.1));
+			System.out.println("\nTOTAL DA COMPRA R$ "+df.format(total * 1.1),"\n");
 			System.out.print("Data: "+formatterData.format(agora)+"\t");
 			System.out.print("Hora: "+formatterHora.format(agora));
 			linhaNota(falso);
@@ -87,18 +87,10 @@ public class DevSaudavel {
 				}
 			}
 			System.out.print("\n\n2X no Cartão");
-			System.out.println("9% de ICMS : R$ "+((total * 0.09),2));
-			System.out.println("15% de acréscimo : R$ "+((total * 0.1),2));
-			System.out.println("\nValor da parcela : R$ "+((total * 1.15),2)/2,"\n");
-			System.out.println("\nTOTAL DA COMPRA R$ " + ((total * 1.1),2),"\n");
-			// data/hora atual
-			LocalDateTime agora = LocalDateTime.now();
-			// formatar a data
-			DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-			String dataFormatada = formatterData.format(agora);
-			// formatar a hora
-			DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
-			String horaFormatada = formatterHora.format(agora);
+			System.out.println("9% de ICMS : R$ "+df.format(total * 0.09));
+			System.out.println("15% de acréscimo : R$ "+df.format(total * 0.1));
+			System.out.println("\nValor da parcela : R$ "+df.format((total * 1.15)/2),"\n");
+			System.out.println("\nTOTAL DA COMPRA R$ " +df.format(total * 1.1),"\n");
 			System.out.print("Data: "+formatterData.format(agora)+"\t");
 			System.out.print("Hora: "+formatterHora.format(agora));
 			linhaNota(falso);
@@ -108,14 +100,14 @@ public class DevSaudavel {
 		auxQuant=0					
 		contador=0
 		for (inteiro x = 0; x < 10; x++) {
-			verificarRepeticao[x] = " "
+			verificarRepeticao[x] = " ";
 		}
 		for (inteiro x = 0; x < 10; x++) {
-			estoque[x] -= carrinhoCompras[x]
+			estoque[x] -= carrinhoCompras[x];
 		}
 		for (inteiro x = 0; x < 10; x++) {
-			carrinhoCompras[x] = 0			
-			total = 0.0
+			carrinhoCompras[x] = 0	;		
+			total = 0.0;
 		}
 	}//chave do public static void main
 	
