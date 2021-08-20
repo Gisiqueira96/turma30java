afe5724069db3a205d03e1816bbf4bc8e847f64a
unknown <giovanna.penedos@outlook.com>
2021-08-20 15:30:01 -0300

    Atualização

--- PI/db_ecommerce.sql
+++ PI/db_ecommerce.sql
@@ -1,30 +1,33 @@
 
-use db_ecommerce_PI;
+create database db_ecommerce_pi;
+use db_ecommerce_pi;
 
 CREATE TABLE `tb_categoria` (
-	`id_categoria` int NOT NULL AUTO_INCREMENT,
+	`id` int NOT NULL AUTO_INCREMENT,
 	`nome_ong` varchar(100) NOT NULL,
 	`local_atuacao` varchar(100) NOT NULL,
 	`tipo` varchar(100) NOT NULL,
-	PRIMARY KEY (`id_categoria`)
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `tb_produto` (
-	`id_produto` int NOT NULL AUTO_INCREMENT,
+	`id` int NOT NULL AUTO_INCREMENT,
 	`nome_produto` varchar(100) NOT NULL,
 	`valor` DECIMAL(9,2) NOT NULL,
 	`estoque` int(4) NOT NULL,
 	`descricao` varchar(255) NOT NULL,
 	`id_categoria` int NOT NULL,
-	PRIMARY KEY (`id_produto`)
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `tb_usuario` (
-	`id_usuario` int NOT NULL AUTO_INCREMENT,
+	`id` int NOT NULL AUTO_INCREMENT,
 	`nome_completo` varchar(100) NOT NULL,
 	`email` varchar(100) NOT NULL UNIQUE,
 	`senha` varchar(100) NOT NULL,
-	PRIMARY KEY (`id_usuario`)
+	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk0` FOREIGN KEY (`id_categoria`) REFERENCES `tb_categoria`(`id_categoria`);
+ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk0` FOREIGN KEY (`id_categoria`) REFERENCES `tb_categoria`(`id`);
+
+
