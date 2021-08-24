a2e0826c0197ed449196794f19879a8f8e3b2015
unknown <giovanna.penedos@outlook.com>
2021-08-24 10:42:41 -0300

    Inicio do Blog Pessoal

---
+++ blogPessoal/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,27 @@
+package org.generation.blogPessoal.controller;
+
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.generation.blogPessoal.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository repositoty;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll() {
+		return ResponseEntity.ok(repositoty.findAll());
+	}
+}
