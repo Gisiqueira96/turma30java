dea8ae6196be6df10f6ec980f33438458ea9355d
Gisiqueira96 <giovanna.penedos@outlook.com>
2021-09-03 20:38:39 -0300

    Adicionado Security no Blog Pessoal

--- blogPessoal/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
+++ blogPessoal/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
@@ -23,36 +23,43 @@ public class UserDetailsImpl implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getPassword() {
+		// TODO Auto-generated method stub
 		return password;
 	}
 
 	@Override
 	public String getUsername() {
+		// TODO Auto-generated method stub
 		return userName;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
